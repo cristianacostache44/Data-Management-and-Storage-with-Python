3a5910efebfd472df96eb524c3f1f0f7f22bda71
Cristiana Costache <cristianacostache44@gmail.com>
2026-08-03 16:12:33 +0300

    random.sample

--- Curs1/Lotto/main.py
+++ Curs1/Lotto/main.py
@@ -2,5 +2,5 @@
 from number import Number
 
 with open("Lotto/numere_extrase.txt","a+") as file_w:
-    continut = file_w.write(str(Number(1,49)) + "\n")
+    continut = file_w.write(str(Number(1,49,6)) + "\n") # 6 = nr de numere UNICE
     
--- Curs1/Lotto/number.py
+++ Curs1/Lotto/number.py
@@ -3,8 +3,8 @@ import random
 
 class Number:
 
-    def __init__(self, a, b):
-        self.nr = random.randrange(a,b)
+    def __init__(self, a, b, c):
+        self.nr = random.sample(range(a,b), c)
 
     def __str__(self):
         return str(self.nr)
